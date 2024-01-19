5089c3b16c0503850029b67943d6db7d17384e09
noobmaster69jr <srashwin22@gmail.com>
2024-01-19 10:39:07 +0530

    query strings

--- src/App.jsx
+++ src/App.jsx
@@ -1,64 +1,73 @@
 /*
-  Create two Routes so that the given navigation works and the proper UI is displayed.
+  Inside the User component you need to parse the query string from the 
+  URL and then show the information about the selected user (which you can 
+    get from the `users` object).
 
-  When the user navigates to /tyler, they should see "Student: tyler".
-  When the user navigates to /invoices/1, they should see "Invoice #1".
+  If no user is selected, show "Select a user".
 */
 
-import * as React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link , useParams} from 'react-router-dom';
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route, Link , useSearchParams} from "react-router-dom";
 
-const Student = () => { 
- const {username} = useParams()
- return <h3>Student: {username}</h3>;
- } 
-
-const Invoice = () => {
-    const {userId} = useParams() 
-    console.log(userId)
-    return <h3>Invoice: {userId}</h3>;
+const users = {
+  tylermcginnis: {
+    name: "Tyler McGinnis",
+    handle: "tylermcginnis",
+    avatar:
+      "https://res.cloudinary.com/uidotdev/image/twitter_name/tylermcginnis"
+  },
+  sarah_edo: {
+    name: "Sarah Drasner",
+    handle: "sarah_edo",
+    avatar: "https://res.cloudinary.com/uidotdev/image/twitter_name/sarah_edo"
+  },
+  ralex1993: {
+    name: "Alex Anderson",
+    handle: "ralex1993",
+    avatar: "https://res.cloudinary.com/uidotdev/image/twitter_name/ralex1993"
+  }
 };
 
-export default function App() {
-    
+function User() { 
+  const [q, setQ] = useSearchParams()
+  
+  const user = q.get("id")
+ console.log(users[user])
 
+ if(user == null){
+  return <div>Select a user</div>
+ }
+return <div>
+  <img src={`${users[user].avatar}`} />
+  <h3>{users[user].name}</h3>
+  <h2>{users[user].handle}</h2>
+</div>
+  
+}
+
+export default function App() {
   return (
     <Router>
-      <h2>Invoices</h2>
-      <ul>
-        <li>
-          <Link to="/invoices/1">1</Link>
-        </li>
-        <li>
-          <Link to="/invoices/2">2</Link>
-        </li>
-        <li>
-          <Link to="/invoices/3">3</Link>
-        </li>
-        <li>
-          <Link to="/invoices/4">4</Link>
-        </li>
-      </ul>
-      <h2>Students</h2>
-      <ul>
-        <li>
-          <Link to="/tyler">Tyler</Link>
-        </li>
-        <li>
-          <Link to="/jake">Jake</Link>
-        </li>
-        <li>
-          <Link to="/mikenzi">Mikenzi</Link>
-        </li>
-      </ul>
+      <div>
+        <h1>Users</h1>
+        <ul>
+          <li>
+            <Link to="/?id=tylermcginnis">Tyler</Link>
+          </li>
+          <li>
+            <Link to="/?id=sarah_edo">Sarah</Link>
+          </li>
+          <li>
+            <Link to="/?id=ralex1993">Alex</Link>
+          </li>
+        </ul>
 
-      <hr />
-      <Routes>
+        <hr />
 
-    <Route path="/invoices/:userId" element={<Invoice/>}></Route>
-    <Route path="/:username" element={<Student/>}></Route>
-      </Routes>
-      {/* Routes go here */}
+        <Routes>
+          <Route path="/" element={<User />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
